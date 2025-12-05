5ad5aa2642876bf2d54b28f90bbdf6c483ebe83a
jasonmishi <jasonmishi@gmail.com>
2025-12-06 00:38:01 +0530

    fix asgardeo static optimizaiton

--- src/app/api/config/route.ts
+++ src/app/api/config/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { asgardeo } from '@/config/appConfig';
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   return NextResponse.json({ asgardeo });
 }
